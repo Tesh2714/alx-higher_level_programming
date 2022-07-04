5a8b23d373c62c7876ed23b119121427e60bb7c4
Tesh2714 <teas38mum@gmail.com>
2022-07-04 11:18:10 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with attribute area"""
+    def area(self):
+        """raises an exception """ 
+        raise Exception("area() is not implemented")
