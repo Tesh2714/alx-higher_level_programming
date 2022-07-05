25d8fc390cba6d22df8374ceca781d5fd3508724
Tesh2714 <teas38mum@gmail.com>
2022-07-05 07:35:32 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,4 +13,5 @@ class Student:
         self.age = age
         
     def to_json(self):
-        """returns a dictionary representation of a Student instance"""                                                                        return self.
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
