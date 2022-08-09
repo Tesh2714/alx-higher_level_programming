a252c7bf059c42d4f683db73d1e009f74cf80fdc
Tesh2714 <teas38mum@gmail.com>
2022-08-09 12:33:21 -0700

    5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- print full decription of the table first_table from the database hbtn_0c_0 in my MySQL server
+-- The database name will be passed as an argument of the mysql command
+
+SHOW CREATE TABLE first_table;
