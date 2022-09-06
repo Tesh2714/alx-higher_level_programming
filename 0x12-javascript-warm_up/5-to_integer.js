9bda97ade26452462ae77b9cabee74b98727e677
Tesh2714 <teas38mum@gmail.com>
2022-09-05 19:59:06 -0700

    5-to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 'use strict';
 let arg = process.argv[2];
 if (isNaN(arg)) {
-	  console.log('Not a number');
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + arg);
+  console.log('My number: ' + arg);
 }
