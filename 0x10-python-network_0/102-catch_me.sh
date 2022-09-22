8328bd6b7972960650d585b5a212cd33d76c5118
Tesh2714 <teas38mum@gmail.com>
2022-09-22 12:27:22 -0700

    102catch

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -sLX PUT -d "user_id=98" -H "Origin:School" 0.0.0.0:5000/catch_me
